621cf8a0290a9c5758786643b7c96dfbf78d2203
Varuzh <159531188+Varuzh21@users.noreply.github.com>
2024-04-05 17:50:36 +0400

    fix

--- app/shop/webapp/controller/DetailDetail.controller.js
+++ app/shop/webapp/controller/DetailDetail.controller.js
@@ -1,6 +1,7 @@
-sap.ui.define(
-  ["sap/ui/core/mvc/Controller", "sap/m/StandardListItem"],
-  function (Controller, StandardListItem) {
+sap.ui.define([
+  "sap/ui/core/mvc/Controller"
+  ],
+  function (Controller) {
     "use strict";
 
     return Controller.extend("com.shop.controller.DetailDetail", {
@@ -70,29 +71,50 @@ sap.ui.define(
     //       .catch(function (oError) {});
     //   },
     
-    onPress: function () {
-        const oModel = this.getView().getModel();
-        const oContext = this.getView().getBindingContext();
-        const parameters = {productid: this.productId};
 
-        const oBindingContext = oModel.createBindingContext("/ShopCart");
+    onPress: async function () {
+      try {
+            const oModel = this.getView().getModel();
+            const oContext = this.getView().getBindingContext();
+            const sProductId = this.productId;
 
-        // const oBindingContext = new sap.ui.model.odata.v4.ODataContextBinding()
-        // oBindingContext.setParameter("path", "/ShopCart");
-        const oBatchRequest = {
-            batchGroupId: "myBatch",
-            changesetId: "myChangeset",
-            requests: [parameters],
-        };
+            const oBinding = this.getView().getModel().createBindingContext('/ShopCart("0e00fee7-f6a7-4fe5-9e00-a6e641af8d68")?$expand=product,user')
 
-        oBatchRequest.requests.push(oBindingContext);
+            // Ensure the binding context is deferred
+            const oBindingContext = await oModel.bindContext('/ShopCart("0e00fee7-f6a7-4fe5-9e00-a6e641af8d68")?$expand=product,user', oBinding, { $$groupId: "myBatch" })
 
-        oModel.submitBatch("myBatch")
-            .then(function (oResponse) {
-            })
-            .catch(function (oError) {
-                console.error("Error occurred while submitting batch request:", oError);
-            });
+            // Set parameters
+            oBindingContext.setParameter("product_ID", sProductId);
+
+            // Execute the action
+            const result = await oBindingContext.execute();
+    
+            console.log(result);
+      } catch (error) {
+            console.error(error);
+      }
+        // const oModel = this.getView().getModel();
+        // const oContext = this.getView().getBindingContext();
+        // const parameters = {productid: this.productId};
+
+        // const oBindingContext = oModel.createBindingContext("/ShopCart");
+
+        // // const oBindingContext = new sap.ui.model.odata.v4.ODataContextBinding()
+        // // oBindingContext.setParameter("path", "/ShopCart");
+        // const oBatchRequest = {
+        //   batchGroupId: "myBatch",
+        //   changesetId: "myChangeset",
+        //   requests: [parameters],
+        // };
+
+        // oBatchRequest.requests.push(oBindingContext);
+
+        // oModel.submitBatch("myBatch")
+        //     .then(function (oResponse) {
+        //     })
+        //     .catch(function (oError) {
+        //         console.error("Error occurred while submitting batch request:", oError);
+        //     });
         },
     });
   }
